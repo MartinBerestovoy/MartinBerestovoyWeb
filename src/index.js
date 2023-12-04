0b85b7669935f9c88594418e9b4d4ede92d01336
Martin Berestovoy <martin.berestovoy@gmail.com>
2023-12-04 01:09:34 -0300

    analytics aded

--- src/index.js
+++ src/index.js
@@ -4,6 +4,10 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
+import { inject } from '@vercel/analytics';
+ 
+inject();
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
