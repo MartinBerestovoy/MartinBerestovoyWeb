f59b125b3b669823be69b33281247d6c7f395455
Berestovoy, Emi <emiliano.berestovoy@accenture.com>
2026-03-18 20:53:43 -0300

    Rebuild

---
+++ main.js
@@ -0,0 +1,5 @@
+// Sticky header
+const header = document.getElementById('main-header');
+window.addEventListener('scroll', () => {
+  header.classList.toggle('nav-scrolled', window.scrollY > 20);
+}, { passive: true });
