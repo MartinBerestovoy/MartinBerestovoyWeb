83fba2bb7b045ee9ddbe9d40ddd51bb44beca13c
Berestovoy, Emi <emiliano.berestovoy@accenture.com>
2026-03-18 22:59:36 -0300

    Total refactor

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 // Sticky header
 const header = document.getElementById('main-header');
 window.addEventListener('scroll', () => {
-  header.classList.toggle('nav-scrolled', window.scrollY > 20);
+    header.classList.toggle('nav-scrolled', window.scrollY > 20);
 }, { passive: true });
